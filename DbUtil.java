71b829f7647871fa037f95565621d906b9709317
Adalberto55 <79849854+Adalberto55@users.noreply.github.com>
2021-05-18 10:40:31 -0300

    Add files via upload

---
+++ DbUtil.java
@@ -0,0 +1,34 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DbUtil {
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection != null)
+            return connection;
+        else {
+            try {
+                Properties prop = new Properties();
+                
+                String user = "postgres";
+                String password = "123456";
+                
+                Class.forName("org.postgresql.Driver");
+              
+                connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/userdb",user, password);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return connection;
+        }
+
+    }
+}
